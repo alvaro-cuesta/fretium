cea1b3a98c31947fed8b0833cf5b0ad7b858adf7
Álvaro Cuesta <1827495+alvaro-cuesta@users.noreply.github.com>
2026-03-20 15:33:52 +0100

    fix: `select` should look like `input`

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './components/App.tsx';
-import './index.css';
+import './index.scss';
 
 const $root = document.getElementById('root');
 if (!$root) {
