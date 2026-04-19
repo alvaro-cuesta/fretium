6c63fd6a3597f1d3f6c12e5eb43787b96c175d5f
Álvaro Cuesta <1827495+alvaro-cuesta@users.noreply.github.com>
2026-04-19 23:08:08 +0200

    feat: major scale positions

--- src/config/patterns/scales.ts
+++ src/config/patterns/scales.ts
@@ -1,21 +1,610 @@
 import type { PatternConfigEntryList } from '../../lib/pattern-config';
-import { SCALE_ROOT_COLOR, SCALE_TONE_COLOR } from './common';
+import { ALT_OPACITY, SCALE_ROOT_COLOR, SCALE_TONE_COLOR } from './common';
 
 export const PATTERNS_SCALES = {
   type: 'optgroup',
   displayName: 'Scales',
   entries: {
     major: {
-      type: 'pattern',
+      type: 'sublist',
       displayName: 'Major scale',
-      rules: [
-        { condition: { interval: '1' }, color: SCALE_ROOT_COLOR },
-        {
-          condition: { interval: ['2', '3', '4', '5', '6', '7'] },
-          color: SCALE_TONE_COLOR,
+      entries: {
+        full: {
+          type: 'pattern',
+          displayName: 'Full',
+          rules: [
+            { condition: { interval: '1' }, color: SCALE_ROOT_COLOR },
+            {
+              condition: { interval: ['2', '3', '4', '5', '6', '7'] },
+              color: SCALE_TONE_COLOR,
+            },
+          ],
+          isFullOctave: true,
         },
-      ],
-      isFullOctave: true,
+        positions: {
+          type: 'optgroup',
+          displayName: 'Positions',
+          entries: {
+            e: {
+              type: 'pattern',
+              displayName: 'E position',
+              rules: [
+                {
+                  condition: { interval: '1', string: 6 },
+                  color: SCALE_ROOT_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['7', '2'],
+                    string: 6,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['3', '4', '5'],
+                    string: 5,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['6', '7'],
+                    string: 4,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['1'],
+                    string: 4,
+                  },
+                  color: SCALE_ROOT_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['2', '3', '4'],
+                    string: 3,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['5', '6'],
+                    string: 2,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['7', '2'],
+                    string: 1,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['1'],
+                    string: 1,
+                  },
+                  color: SCALE_ROOT_COLOR,
+                },
+              ],
+            },
+            ed: {
+              type: 'pattern',
+              displayName: 'E-D position',
+              rules: [
+                {
+                  condition: { interval: '1', string: 6 },
+                  color: SCALE_ROOT_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['2', '3'],
+                    string: 6,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['4', '5', '6'],
+                    string: 5,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['7', '2'],
+                    string: 4,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['1'],
+                    string: 4,
+                  },
+                  color: SCALE_ROOT_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['3', '4'],
+                    string: 3,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['5'],
+                    string: 3,
+                  },
+                  color: SCALE_TONE_COLOR,
+                  opacity: ALT_OPACITY,
+                },
+                {
+                  condition: {
+                    interval: ['5'],
+                    string: 2,
+                  },
+                  color: SCALE_TONE_COLOR,
+                  opacity: ALT_OPACITY,
+                },
+                {
+                  condition: {
+                    interval: ['6', '7'],
+                    string: 2,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['2', '3'],
+                    string: 1,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['1'],
+                    string: 1,
+                  },
+                  color: SCALE_ROOT_COLOR,
+                },
+              ],
+            },
+            d: {
+              type: 'pattern',
+              displayName: 'D position',
+              rules: [
+                {
+                  condition: {
+                    interval: ['2', '3', '4'],
+                    string: 6,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['5', '6'],
+                    string: 5,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['7'],
+                    string: 5,
+                  },
+                  color: SCALE_TONE_COLOR,
+                  opacity: ALT_OPACITY,
+                },
+                {
+                  condition: {
+                    interval: ['7'],
+                    string: 4,
+                  },
+                  color: SCALE_TONE_COLOR,
+                  opacity: ALT_OPACITY,
+                },
+                {
+                  condition: {
+                    interval: ['1'],
+                    string: 4,
+                  },
+                  color: SCALE_ROOT_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['2'],
+                    string: 4,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['3'],
+                    string: 4,
+                  },
+                  color: SCALE_TONE_COLOR,
+                  opacity: ALT_OPACITY,
+                },
+                {
+                  condition: {
+                    interval: ['3'],
+                    string: 3,
+                  },
+                  color: SCALE_TONE_COLOR,
+                  opacity: ALT_OPACITY,
+                },
+                {
+                  condition: {
+                    interval: ['4', '5'],
+                    string: 3,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['6', '7'],
+                    string: 2,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['1'],
+                    string: 2,
+                  },
+                  color: SCALE_ROOT_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['2', '3', '4'],
+                    string: 1,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+              ],
+            },
+            c: {
+              type: 'pattern',
+              displayName: 'C position',
+              rules: [
+                {
+                  condition: {
+                    interval: ['3', '4', '5'],
+                    string: 6,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['6', '7'],
+                    string: 5,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['1'],
+                    string: 5,
+                  },
+                  color: SCALE_ROOT_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['2', '3', '4'],
+                    string: 4,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['5', '6'],
+                    string: 3,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['7'],
+                    string: 3,
+                  },
+                  color: SCALE_TONE_COLOR,
+                  opacity: ALT_OPACITY,
+                },
+                {
+                  condition: {
+                    interval: ['7'],
+                    string: 2,
+                  },
+                  color: SCALE_TONE_COLOR,
+                  opacity: ALT_OPACITY,
+                },
+                {
+                  condition: {
+                    interval: ['1'],
+                    string: 2,
+                  },
+                  color: SCALE_ROOT_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['2'],
+                    string: 2,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['3', '4', '5'],
+                    string: 1,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+              ],
+            },
+            a: {
+              type: 'pattern',
+              displayName: 'A position',
+              rules: [
+                {
+                  condition: {
+                    interval: ['4', '5', '6'],
+                    string: 6,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['7'],
+                    string: 5,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['1'],
+                    string: 5,
+                  },
+                  color: SCALE_ROOT_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['2'],
+                    string: 5,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['3', '4', '5'],
+                    string: 4,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['6', '7'],
+                    string: 3,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['1'],
+                    string: 3,
+                  },
+                  color: SCALE_ROOT_COLOR,
+                  opacity: ALT_OPACITY,
+                },
+                {
+                  condition: {
+                    interval: ['1'],
+                    string: 2,
+                  },
+                  color: SCALE_ROOT_COLOR,
+                  opacity: ALT_OPACITY,
+                },
+                {
+                  condition: {
+                    interval: ['2', '3'],
+                    string: 2,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['4', '5', '6'],
+                    string: 1,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+              ],
+            },
+            ag: {
+              type: 'pattern',
+              displayName: 'A-G position',
+              rules: [
+                {
+                  condition: {
+                    interval: ['5', '6', '7'],
+                    string: 6,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['1'],
+                    string: 5,
+                  },
+                  color: SCALE_ROOT_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['2', '3'],
+                    string: 5,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['4', '5', '6'],
+                    string: 4,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['7'],
+                    string: 3,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['1'],
+                    string: 3,
+                  },
+                  color: SCALE_ROOT_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['2'],
+                    string: 3,
+                  },
+                  color: SCALE_TONE_COLOR,
+                  opacity: ALT_OPACITY,
+                },
+                {
+                  condition: {
+                    interval: ['2'],
+                    string: 2,
+                  },
+                  color: SCALE_TONE_COLOR,
+                  opacity: ALT_OPACITY,
+                },
+                {
+                  condition: {
+                    interval: ['3', '4'],
+                    string: 2,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['5', '6', '7'],
+                    string: 1,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+              ],
+            },
+            g: {
+              type: 'pattern',
+              displayName: 'G position',
+              rules: [
+                {
+                  condition: {
+                    interval: ['6', '7'],
+                    string: 6,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['1'],
+                    string: 6,
+                  },
+                  color: SCALE_ROOT_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['2', '3', '4'],
+                    string: 5,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['5', '6'],
+                    string: 4,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['7'],
+                    string: 4,
+                  },
+                  color: SCALE_TONE_COLOR,
+                  opacity: ALT_OPACITY,
+                },
+                {
+                  condition: {
+                    interval: ['7'],
+                    string: 3,
+                  },
+                  color: SCALE_TONE_COLOR,
+                  opacity: ALT_OPACITY,
+                },
+                {
+                  condition: {
+                    interval: ['1'],
+                    string: 3,
+                  },
+                  color: SCALE_ROOT_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['2'],
+                    string: 3,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['3'],
+                    string: 3,
+                  },
+                  color: SCALE_TONE_COLOR,
+                  opacity: ALT_OPACITY,
+                },
+                {
+                  condition: {
+                    interval: ['3'],
+                    string: 2,
+                  },
+                  color: SCALE_TONE_COLOR,
+                  opacity: ALT_OPACITY,
+                },
+                {
+                  condition: {
+                    interval: ['4', '5'],
+                    string: 2,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['6', '7'],
+                    string: 1,
+                  },
+                  color: SCALE_TONE_COLOR,
+                },
+                {
+                  condition: {
+                    interval: ['1'],
+                    string: 1,
+                  },
+                  color: SCALE_ROOT_COLOR,
+                },
+              ],
+            },
+          },
+        },
+      },
     },
     minor: {
       type: 'pattern',
